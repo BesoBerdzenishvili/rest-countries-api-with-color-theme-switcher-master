67df471a09ed5f10acef20f61296eeea5d85fffa
fliderman01 <fliderman01@gmail.com>
2023-07-03 11:46:11 +0400

    feat: Header

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>Where in the world?</h1>
+      <p>Dark Mode</p>
+    </header>
+  );
+}
+
+export default Header;
