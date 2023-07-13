3b8a3499ba2a89298352944e4853a49ca26be99f
fliderman01 <fliderman01@gmail.com>
2023-07-13 19:14:08 +0400

    feat: dark mode

---
+++ components/DarkMode.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import style from "./DarkMode.module.css";
+import moon from "../public/moon_icon.png";
+import sun from "../public/sun_icon.png";
+import Image from "next/image";
+
+const DarkMode = () => {
+  const [darkTheme, setDarkTheme] = useState(() => {
+    const saved = localStorage.getItem("darkTheme");
+    return saved ? JSON.parse(saved) : false;
+  });
+
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    localStorage.setItem("darkTheme", JSON.stringify(darkTheme));
+  }, [darkTheme]);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  useEffect(() => {
+    setTheme(darkTheme ? "dark" : "light");
+  }, [darkTheme]);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const toggleDarkMode = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <button onClick={toggleDarkMode} className={style.DarkModeBtn}>
+      <Image src={darkTheme ? moon : sun} width={20} height={20} />
+      {theme.charAt(0).toUpperCase() + theme.slice(1)} Mode
+    </button>
+  );
+};
+
+export default DarkMode;
